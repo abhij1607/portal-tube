78f8a80a057d14e821a4595b4829674048a4fdd2
Abhi Jain <abhijain9807@gmail.com>
2022-05-05 02:59:54 +0530

    [styles] - sidebar responsiveness

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,35 +4,35 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <aside className="sidebar-container box-shadow">
-      <nav className="navigation aside-nav pd-x-xl">
-        <ul className="list-non-bullets">
+      <nav className="navigation">
+        <ul className="list-non-bullets aside-list">
           <li className="nav-list pd-y-base">
-            <Link to="/">
+            <Link className="link" to="/">
               {" "}
               <i className="fa fa-home" aria-hidden="true" />
               Home
             </Link>
           </li>
           <li className="nav-list pd-y-base">
-            <Link to="/">
+            <Link className="link" to="/">
               <i className="fa fa-folder-open" aria-hidden="true" />
               Playlist
             </Link>
           </li>
           <li className="nav-list pd-y-base">
-            <Link to="/">
+            <Link className="link" to="/">
               <i className="fa fa-thumbs-up" aria-hidden="true" />
               Liked
             </Link>
           </li>
           <li className="nav-list pd-y-base">
-            <Link to="/">
+            <Link className="link" to="/">
               <i className="fa fa-bookmark" aria-hidden="true" />
               Watch Later
             </Link>
           </li>
           <li className="nav-list pd-y-base">
-            <Link to="/">
+            <Link className="link" to="/">
               <i className="fa fa-history" aria-hidden="true" />
               History
             </Link>
