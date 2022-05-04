0f16733270e41794ecaa4353ca50434aa8b8cf7e
Abhi Jain <abhijain9807@gmail.com>
2022-05-04 16:37:36 +0530

    [feat] - NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import "./App.css";
+// import "./App.css";
+import "./styles/index.css";
+import { Nav } from "./components/Navbar/nav";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
 }
 
 export default App;
