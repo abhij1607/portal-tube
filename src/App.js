41454e4ca8968ba03c5706933f6d86fc01166665
Abhi Jain <abhijain9807@gmail.com>
2022-05-05 15:27:24 +0530

    [feat] - hompage UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./styles/index.css";
 import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./components/AppLayout/AppLayout";
+import { Home } from "./pages/Home/Home";
+
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<AppLayout />} />
+        <Route path="/" element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
       </Routes>
     </>
   );
