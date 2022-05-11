07a0539fec04d3b861b13dcfc857d400a88e6490
Abhi Jain <abhijain9807@gmail.com>
2022-05-11 17:24:50 +0530

    feat - login screen

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles/index.css";
 import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./components/AppLayout/AppLayout";
 import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Route>
       </Routes>
     </>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,84 @@
+import "./login.css";
+import { useState } from "react";
+
+const testCredentials = {
+  email: "adarshbalika@gmail.com",
+  password: "adarshBalika123",
+};
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    //perform login
+  };
+
+  return (
+    <main className="align-center">
+      <div className="box-shadow form-box auto-container align-center">
+        <h1 className="h2">Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="input-box">
+            <label className="label p-md txt-wt-md" htmlFor="email">
+              Enter your email:
+            </label>
+            <input
+              className="input-border input"
+              placeholder="Email *"
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="input-box">
+            <label className="label p-md txt-wt-md" htmlFor="pwd">
+              Password:
+            </label>
+            <input
+              className="input-border input"
+              placeholder="Password*"
+              type="password"
+              id="pwd"
+              name="pwd"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="input-checkbox">
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
+            <a href="#" className="align-right">
+              Forgot your Password
+            </a>
+          </div>
+          <button
+            className="btn btn-fill btn-primary btn-lg"
+            type="submit"
+            value="Submit"
+          >
+            Submit
+          </button>
+          <button
+            className="btn btn-fill btn-primary btn-lg mg-y-base"
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              setEmail(testCredentials.email);
+              setPassword(testCredentials.password);
+            }}
+          >
+            Fill in test credentials
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export { Login };
