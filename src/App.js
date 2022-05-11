54f6610057ba2ce956b88c188e38aa22bc4f6d32
Abhi Jain <abhijain9807@gmail.com>
2022-05-12 01:16:11 +0530

    [feat] - RequiresAuth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { AppLayout } from "./components/AppLayout/AppLayout";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
+// import { RequiresAuth } from "./RequireAuth/RequiresAuth";
 
 function App() {
   return (
