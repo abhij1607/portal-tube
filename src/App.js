bfab72a3afdadd0109fa6646a1504ff6f702b26d
Abhi Jain <abhijain9807@gmail.com>
2022-05-15 21:06:16 +0530

    [update] - requiresAuth route

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Login } from "./pages/Login/Login";
 import { SignUp } from "./pages/Signup/Signup";
 import { Playlist } from "./pages/Playlist/Playlist";
 import { SinglePlaylist } from "./pages/SinglePlaylist/SinglePlaylist";
-// import { RequiresAuth } from "./RequireAuth/RequiresAuth";
+import { RequiresAuth } from "./RequireAuth/RequiresAuth";
 
 function App() {
   return (
@@ -16,8 +16,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/playlist" element={<Playlist />} />
-          <Route path="/playlist/:playlistid" element={<SinglePlaylist />} />
+          <Route element={<RequiresAuth />}>
+            <Route path="/playlist" element={<Playlist />} />
+            <Route path="/playlist/:playlistid" element={<SinglePlaylist />} />
+          </Route>
         </Route>
       </Routes>
     </>
--- src/RequireAuth/RequiresAuth.js
+++ src/RequireAuth/RequiresAuth.js
@@ -1,13 +1,15 @@
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, Outlet } from "react-router-dom";
 import { useAuth } from "../context/auth-context";
 
-export function RequiresAuth({ children }) {
-  let location = useLocation();
+const RequiresAuth = () => {
+  const location = useLocation();
   const { userState } = useAuth();
 
   return userState.userToken ? (
-    children
+    <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
   );
-}
+};
+
+export { RequiresAuth };
