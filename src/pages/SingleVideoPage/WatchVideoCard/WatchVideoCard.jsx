7b74447dca6a3610c06850651aa17e062968ae3d
Abhi Jain <abhijain9807@gmail.com>
2022-05-17 13:53:09 +0530

    [feat] - ReactPlayer

--- src/pages/SingleVideoPage/WatchVideoCard/WatchVideoCard.jsx
+++ src/pages/SingleVideoPage/WatchVideoCard/WatchVideoCard.jsx
@@ -1,18 +1,47 @@
 import "./WatchVideoCard.css";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player/youtube";
+// import { useData } from "../../../context/data-context";
+import { fetchVideo } from "../../../utils/server-request";
+import { getNumberInFormat } from "../../../utils/number-format";
 
 const WatchVideoCard = () => {
+  const [video, setVideo] = useState({});
+  const { watchid } = useParams();
+
+  // const {
+  //   dataState: { videos },
+  // } = useData();
+
+  useEffect(() => {
+    (async () => {
+      const currentVideo = await fetchVideo(watchid);
+      setVideo(currentVideo);
+    })();
+  }, []);
+
+  // const video = videos.find((video) => video._id === watchid);
+  console.log(video);
   return (
     <div className="card-container watch-video-card">
-      <img
-        className="card-img-top"
-        src="https://picsum.photos/600/400"
-        alt="card-img"
-      />
+      <div className="player-wrapper">
+        <ReactPlayer
+          className="react-player"
+          url={`https://www.youtube.com/watch?v=${watchid}`}
+          controls={true}
+          playing={true}
+          width="100%"
+          height="100%"
+        />
+      </div>
       <div className="card-info">
-        <h1 className="card-title video-title">Our Changing Planet</h1>
+        <h1 className="card-title video-title">{video.title}</h1>
       </div>
       <div className="card-action">
-        <span className="alight-left">Views</span>
+        <span className="alight-left">
+          {getNumberInFormat(video.views)} views
+        </span>
         <div className="align-right flex-row gap-2">
           <button className="action-icon">
             <i className="fa fa-heart" /> Like
@@ -26,10 +55,7 @@ const WatchVideoCard = () => {
         </div>
       </div>
       <div className="card-body">
-        <p className="description">
-          Visit ten places on our planet that are undergoing the biggest changes
-          today.
-        </p>
+        <p className="description">{video.description}</p>
       </div>
     </div>
   );
