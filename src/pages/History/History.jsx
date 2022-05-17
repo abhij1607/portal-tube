42fe9499c15c8c94e66dc81f02ffcc5793a86f54
Abhi Jain <abhijain9807@gmail.com>
2022-05-18 01:02:38 +0530

    [feat] - history page

---
+++ src/pages/History/History.jsx
@@ -0,0 +1,40 @@
+import { HistoryVideoCard } from "./HistoryVideoCard/HistoryVideoCard";
+import { useAuth } from "../../context/auth-context";
+import { requestDeleteAllHistory } from "../../utils/server-request";
+
+const History = () => {
+  const {
+    userState: {
+      userDetails: { history },
+      userToken,
+    },
+    userDispatch,
+  } = useAuth();
+
+  const headers = { headers: { authorization: userToken } };
+
+  const handleDeleteAllHistory = () => {
+    requestDeleteAllHistory(headers, userDispatch);
+  };
+  return (
+    <main>
+      <div className="flex-row item-center">
+        <h1 className="align-left">History Videos</h1>
+        <button
+          className="align-right mg-x-xl txt-xl btn"
+          onClick={handleDeleteAllHistory}
+        >
+          Clear All History
+        </button>
+      </div>
+
+      <div className="single-playlist-layout">
+        {history.map((video) => (
+          <HistoryVideoCard key={video._id} video={video} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export { History };
