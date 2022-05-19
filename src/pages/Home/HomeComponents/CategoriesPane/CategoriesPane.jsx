75b6b271108088d45114d2add3698084ffbb4483
Abhi Jain <abhijain9807@gmail.com>
2022-05-19 22:53:32 +0530

    [style][update] - categories responsiveness

--- src/pages/Home/HomeComponents/CategoriesPane/CategoriesPane.jsx
+++ src/pages/Home/HomeComponents/CategoriesPane/CategoriesPane.jsx
@@ -1,3 +1,4 @@
+import "./CategoriesPane.css";
 import { useData } from "../../../../context/data-context";
 
 const CategoriesPane = () => {
@@ -7,45 +8,43 @@ const CategoriesPane = () => {
   } = useData();
 
   return (
-    <aside className="pd-y-base pd-x-sm">
-      <ul className="flex-row gap-2">
-        <li>
-          <button
-            className={`btn ${
-              selectedCategory === "All" ? "btn-primary" : "btn-secondary"
-            }`}
-            onClick={() =>
-              dataDispatch({
-                type: "SELECT_CATEGORY",
-                payload: "All",
-              })
-            }
-          >
-            All
-          </button>
-        </li>
-        {categories.map((category) => {
-          return (
-            <li key={category._id}>
-              <button
-                className={`btn ${
-                  selectedCategory === category.categoryName
-                    ? "btn-primary"
-                    : "btn-secondary"
-                }`}
-                onClick={() =>
-                  dataDispatch({
-                    type: "SELECT_CATEGORY",
-                    payload: category.categoryName,
-                  })
-                }
-              >
-                {category.categoryName}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+    <aside className="pd-y-base pd-x-sm flex-row gap-2 categories">
+      <li>
+        <button
+          className={`btn ${
+            selectedCategory === "All" ? "btn-primary" : "btn-secondary"
+          }`}
+          onClick={() =>
+            dataDispatch({
+              type: "SELECT_CATEGORY",
+              payload: "All",
+            })
+          }
+        >
+          All
+        </button>
+      </li>
+      {categories.map((category) => {
+        return (
+          <li key={category._id}>
+            <button
+              className={`btn wd-max ${
+                selectedCategory === category.categoryName
+                  ? "btn-primary"
+                  : "btn-secondary"
+              }`}
+              onClick={() =>
+                dataDispatch({
+                  type: "SELECT_CATEGORY",
+                  payload: category.categoryName,
+                })
+              }
+            >
+              {category.categoryName}
+            </button>
+          </li>
+        );
+      })}
     </aside>
   );
 };
